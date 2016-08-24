25374c4985066d7792e95cb35ca3aa98fda69e67
Wolfgang Groß <bitbucket@text-mode.de>
2016-08-24 18:08:22 +0200

    Tests finished

--- 1.0/NLog.psm1
+++ 1.0/NLog.psm1
@@ -1,56 +1,28 @@
 # Load/Install NLog.Dll
 
-function Get-NLogDllPath {
-    Get-ChildItem $PSScriptRoot\*\NLog.Dll -File -Recurse | Sort-Object ($_.Path) -Descending | Select-Object -First 1
+function Get-NLogDll {
+    Get-ChildItem $PSScriptRoot -Filter "NLog.Dll" -File -Recurse | Where-Object { $_.FullName.Contains("net45") }
 }
 
-$NLogDllPath = Get-NLogDllPath
+$script:NLogDll = Get-NLogDll
 
-if(!$positionOfCursor -or !(Test-Path $NLogDllPath)) {
+if(!($script:NLogDll) -or !(Test-Path $NLogDll)) {
     
     # Try to reinstall NLog
 
     $nuget = Get-Command nuget.exe -ErrorAction SilentlyContinue
-    
     if(!($nuget)) {
         throw "Module NLog requires nuget.exe in path to reload NLog from Nuget.Org"
     }
     
     nuget.exe install NLog
+
+    $script:NLogDll = Get-NLogDll
 }
 
-return 
+# Load Dll
 
-#region Initialization of this module
-if(Test-Path "$PSScriptRoot\NLog.4.0.0\lib\net45\NLog.dll") {
-    
-    Write-Host "Loading NLog 4.0.0.0..."
-
-    Add-Type -Path "$PSScriptRoot\NLog.4.0.0\lib\net45\NLog.dll"
-
-} elseif((Test-Path "$PSScriptRoot\NLog.3.1.0.0")) {
-    
-    Write-Host "Loading NLog 3.1.0.0..."
-
-    Add-Type -Path "$PSScriptRoot\NLog.3.1.0.0\NLog.dll"
-
-} elseif((Test-Path "$PSScriptRoot\NLog.2.1.0.0")) {
-
-    Write-Host "Loading NLog 2.1.0.0..."
-
-    Add-Type -Path "$PSScriptRoot\NLog.2.1.0.0\NLog.dll"
-    Add-Type -Path "$PSScriptRoot\NLog.2.1.0.0\MongoDB.Bson.dll"
-    Add-Type -Path "$PSScriptRoot\NLog.2.1.0.0\MongoDB.Driver.dll"
-    Add-Type -Path "$PSScriptRoot\NLog.2.1.0.0\NLog.Mongo.dll"
-    
-    $defaultConfig = "$PSScriptRoot\NLog.2.1.0.0\default.config.xml"
-
-} elseif((Test-Path "$PSScriptRoot\NLog.2.0.1.2")) {
-    
-    Write-Host "Loading NLog 2.0.1.2..."
-
-    Add-Type -Path "$PSScriptRoot\NLog.2.0.1.2\NLog.dll"
-}
+Add-Type -Path $script:NLogDll.FullName
 
 function Initialize-NLogInternalLogging{    param(        $LogFilePath = "$PSScriptRoot\nlog.internal.log",        [ValidateSet("Trace","Debug","Info","Warning","Error","Fatal")]        $LogLevelName = "Trace"    )    process {        $Env:NLOG_INTERNAL_LOG_FILE  = $LogFilePath        $Env:NLOG_INTERNAL_LOG_LEVEL  = $LogLevelName    }}
 
@@ -63,7 +35,8 @@ function Get-Logger {
     .SYNOPSIS
         Provide a Logger instance to send data to NLog.
     #>
-    [CmdletBinding(SupportsShouldProcess=$false,SupportsPaging=$false,DefaultParameterSetName="byName",ConfirmImpact=[System.Management.Automation.ConfirmImpact]::None)]
+    [CmdletBinding(DefaultParameterSetName="byName")]
+    [OutputType([NLog.Logger])]
     param(
         [Parameter(ParameterSetName="byName",Mandatory=$true,Position=0,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false,ValueFromRemainingArguments=$false,DontShow=$false,HelpMessage="none")]
         [ValidateNotNullOrEmpty()]
--- 1.0/NLogTestImpl.ps1
+++ 1.0/NLogTestImpl.ps1
@@ -2,9 +2,34 @@ Import-Module Pester
 
 Describe "NLog initialization" {
     BeforeAll {
-        Get-ChildItem $PSScriptRoot/NLog* -Directory | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+        Get-ChildItem $PSScriptRoot\NLog* -Directory | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
     }
+
     It "Reloads the NLog nuget package if the DLL is missing" {
         Import-Module $PSScriptRoot\NLog.psm1
-    }    
+    }
+
+    It "Finds a Nlog.Dll comoatible wit .Net 4.5" {
+        $nlogDir = Get-ChildItem $PSScriptRoot\NLog* -Directory
+        $nlogDir | Should Not Be $null
+        Get-ChildItem ($nlogDir.FullName) -Filter "NLog.Dll" -File -Recurse | Where-Object { $_.FullName.Contains("net45") } | Should Not Be $null
+    }
+
+    It "Loads NLog and provides a LogManager type" {
+        [type]"NLog.LogManager" | Should Not Be $null
+    }
 }
+
+Describe "Get-Logger" {
+    
+    Import-Module $PSScriptRoot\NLog.psm1 -Force
+    
+    It "Returns a logger named by properties of the scrot invocation" {
+        (Get-Logger -ScriptInvocation $MyInvocation).Name | Should be $MyInvocation.ScriptName
+    }
+
+    It "Returns a logger explicitely named" {
+        (Get-Logger -Name "loggerName").Name | Should be "loggerName"
+    }
+}
+
